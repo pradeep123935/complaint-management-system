a445c0d6852d15a5fc2894ff59664721d5db4d24
pradeep <pradeeppolimetla8466@gmail.com>
2023-11-02 18:28:36 +0530

    add complaint

--- frontend/src/pages/AddComplaint.jsx
+++ frontend/src/pages/AddComplaint.jsx
@@ -1,9 +1,37 @@
 import styles from "./add-complaint.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
 const AddComplaint = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const body = {
+      title,
+      description,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/complaint/addComplaint",
+        body
+      );
+      toast.success(response?.data?.message);
+      navigate("/dashboard");
+    } catch (err) {
+      toast.error(err?.response?.data?.message);
+    }
+  };
+
   return (
     <>
       <h1 className={styles.heading}>Add Complaint</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={submitHandler}>
         <div className={styles.formGroup}>
           <label htmlFor="title" className={styles.label}>
             Title
@@ -13,6 +41,8 @@ const AddComplaint = () => {
             name="title"
             id="title"
             placeholder="Short info about complaint in 6-7 words"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className={styles.input}
           />
         </div>
@@ -25,6 +55,8 @@ const AddComplaint = () => {
             id="description"
             placeholder="Explain bref about the complaint"
             rows={6}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             className={styles.input}
           />
         </div>
